cc71b63fac1d0ff17c470f22e0377afc73bd834c
Rajith Shaji <nag.samayam@gmail.com>
2017-01-22 05:05:47 +0000

    Code clean

--- app/Repositories/ArticleRepository.php
+++ app/Repositories/ArticleRepository.php
@@ -32,19 +32,19 @@ class ArticleRepository extends BaseRepository {
 	 * @return Illuminate\Database\Eloquent\Builder
 	 */
 	private function queryActiveOrderByDate($topicId, $type, $orderBy = 'created_at', $sort = 'desc') {
-		$query = $this->model->select ( 'id', 'created_at as postedOn', 'updated_at', 'title', 'source_url as sourceUrl', 'author_id as authorId', 'description', 'sequence', 'file_path', 'video_url as videoUrl', 'type_title as articleType', 'author_name as authorName', 'author_location as authorLocation', 'author_organization as authorOffice', 'author_designation as authorDesignation', 'upvotes_count as upvoteCount' )->where ( 'topic_id', $topicId );
+		$query = $this->model->select ( 'id', 'created_at as postedOn', 'updated_at', 'title', 'source_url as sourceUrl', 'author_id as authorId', 'description', 'top10_order', 'file_path', 'video_url as videoUrl', 'type_title as articleType', 'author_name as authorName', 'author_location as authorLocation', 'author_organization as authorOffice', 'author_designation as authorDesignation', 'upvotes_count as upvoteCount' )->where ( 'topic_id', $topicId );
 		if ($type === 'top-10' or $type === 'latest') {
 			return $query->orderBy ( $orderBy, $sort );
 		}
 		return $query->where ( 'type_title', $type )->orderBy ( $orderBy, $sort );
 	}
 	private function queryActiveOrderByLatest($topicId, $type, $orderBy = 'latest_order', $sort = 'desc') {
-		$query = $this->model->select ( 'id', 'created_at as postedOn', 'updated_at', 'title', 'source_url as sourceUrl', 'author_id as authorId', 'description', 'sequence', 'file_path', 'video_url as videoUrl', 'type_title as articleType', 'author_name as authorName', 'author_location as authorLocation', 'author_organization as authorOffice', 'author_designation as authorDesignation', 'upvotes_count as upvoteCount' )->where ( 'topic_id', $topicId );
+		$query = $this->model->select ( 'id', 'created_at as postedOn', 'updated_at', 'title', 'source_url as sourceUrl', 'author_id as authorId', 'description', 'top10_order', 'file_path', 'video_url as videoUrl', 'type_title as articleType', 'author_name as authorName', 'author_location as authorLocation', 'author_organization as authorOffice', 'author_designation as authorDesignation', 'upvotes_count as upvoteCount' )->where ( 'topic_id', $topicId );
 		
 		return $query->orderBy ( $orderBy, $sort );
 	}
-	private function queryActiveOrderByTop($topicId, $type, $orderBy = 'sequence', $sort = 'desc') {
-		$query = $this->model->select ( 'id', 'created_at as postedOn', 'updated_at', 'title', 'source_url as sourceUrl', 'author_id as authorId', 'description', 'sequence', 'file_path', 'video_url as videoUrl', 'type_title as articleType', 'author_name as authorName', 'author_location as authorLocation', 'author_organization as authorOffice', 'author_designation as authorDesignation', 'upvotes_count as upvoteCount' )->where ( 'topic_id', $topicId );
+	private function queryActiveOrderByTop($topicId, $type, $orderBy = 'top10_order', $sort = 'desc') {
+		$query = $this->model->select ( 'id', 'created_at as postedOn', 'updated_at', 'title', 'source_url as sourceUrl', 'author_id as authorId', 'description', 'top10_order', 'file_path', 'video_url as videoUrl', 'type_title as articleType', 'author_name as authorName', 'author_location as authorLocation', 'author_organization as authorOffice', 'author_designation as authorDesignation', 'upvotes_count as upvoteCount' )->where ( 'topic_id', $topicId );
 		
 		return $query->orderBy ( $orderBy, $sort );
 	}
@@ -59,7 +59,7 @@ class ArticleRepository extends BaseRepository {
 	 */
 	public function indexBytype($n, $topicId, $type = 'top-ten') {
 		if ($type === 'top-10') {
-			$query = $this->queryActiveOrderByTop ( $topicId, $type, 'sequence' );
+			$query = $this->queryActiveOrderByTop ( $topicId, $type, 'top10_order' );
 		} elseif ($type === 'latest') {
 			$query = $this->queryActiveOrderByLatest ( $topicId, $type );
 		} else {
@@ -79,7 +79,7 @@ class ArticleRepository extends BaseRepository {
 	 */
 	public function index($n, $topicId, $type = 'top-ten') {
 		if ($type === 'top-10') {
-			$query = $this->queryActiveOrderByDate ( $topicId, $type, 'sequence' );
+			$query = $this->queryActiveOrderByDate ( $topicId, $type, 'top10_order' );
 		} elseif ($type === 'latest') {
 			$query = $this->queryActiveOrderByDate ( $topicId, $type );
 		} else {
@@ -125,6 +125,7 @@ class ArticleRepository extends BaseRepository {
 	 */
 	public function saveArticle($article, $inputs, $userId = null) {
 		$article->topic_id = ( int ) $inputs ['topic_id'];
+                 $article->source_url = $inputs['source_url'] ?? null;
 		$article->type_title = $inputs ['type_title'];
 		$article->title = ucwords ( strtolower ( $inputs ['title'] ) );
 		$article->description = ucwords ( $inputs ['description'] );
